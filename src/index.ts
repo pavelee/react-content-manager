79f923ce0c60e7fbf892c9f2cfdcf2c3427e7d65
Paweł Ciosek <ciosek.pawel@gmail.com>
2024-01-07 21:57:34 +0100

    Update package.json and add CmHostHandler

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { CmConfig } from "./services/CmConfig";
 export { CMProvider } from "./pages/CMPage";
 export { CMComponent } from "./components/component/CMComponent";
+export { CmHostHandler } from "./services/CmHostHandler";

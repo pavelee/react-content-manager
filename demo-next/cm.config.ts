609cd8618249b25cf255e891145f9550d356751a
Paweł Ciosek <ciosek.pawel@gmail.com>
2024-05-29 21:54:40 +0200

    chore: Update fetcher to include cache control header

--- demo-next/cm.config.ts
+++ demo-next/cm.config.ts
@@ -2,7 +2,11 @@ import { CmConfig, CmHostHandler } from "react-content-manager";
 
 const fetcher = async (componentId: string): Promise<any> => {
   let host = CmHostHandler.getHost();
-  const data = await fetch(`${host}/api/store?configId=${componentId}`);
+  const data = await fetch(`${host}/api/store?configId=${componentId}`,
+    {
+      cache: "no-cache",
+    }
+  );
   const json = await data.json();
   return json;
 };

7eaaa656b580160812c9a6795978e8a1279446c8
Paweł Ciosek <ciosek.pawel@gmail.com>
2024-10-26 23:31:35 +0200

    refactor: Change searchParams to a Promise in Home function and update dev script to include NODE_OPTIONS for debugging

--- demo-next/app/page.tsx
+++ demo-next/app/page.tsx
@@ -4,19 +4,19 @@ import { CMComponent, CMProvider } from 'react-content-manager'
 type mode = "edit" | "view";
 
 type props = {
-  searchParams: {
+  searchParams: Promise<{
     mode: mode
-  }
+  }>
 }
 
-export default function Home(props: props) {
-  const { mode = 'view' } = props.searchParams;
+export default async function Home(props: props) {
+  const { mode = 'view' } = (await props.searchParams);
   return (
     <CMProvider mode={mode}>
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <CMComponent configId="main_top1" componentId={"container"} mode={mode} />
         <CMComponent configId="main_top" componentId={"container"} mode={mode} />
       </main>
-    </CMProvider>
+    // </CMProvider>
   )
 }

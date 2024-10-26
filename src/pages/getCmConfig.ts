a8fc94fabb293a43ed69759fd6b484979067b7bd
Paweł Ciosek <ciosek.pawel@gmail.com>
2024-10-26 23:31:39 +0200

    refactor: Update getCmConfig to import cmConfig directly and remove obsolete require, adding ts-expect-error for missing types

--- src/pages/getCmConfig.ts
+++ src/pages/getCmConfig.ts
@@ -1,5 +1,12 @@
 import { CmConfig } from "../services/CmConfig";
+// @ts-expect-error - no types for cmConfig
+import cmConfig from "../../../demo-next/cm.config.ts";
 
 export const getCmConfig = (): CmConfig => {
-  return require("../../../demo-next/cm.config.ts").default;
+  return cmConfig;
 };
+
+// export const getCmConfig = (): CmConfig => {
+//   // console.log(require("../../../demo-next/cm.config.ts"));
+//   return require("../../../demo-next/cm.config.ts").default;
+// };

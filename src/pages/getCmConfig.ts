2f40183d4b3ca976214f7de7acd0ce13f38a1281
Paweł Ciosek <ciosek.pawel@gmail.com>
2024-10-27 21:49:09 +0100

    refactor: Simplify cmConfig import in getCmConfig and remove outdated commented code for better readability

--- src/pages/getCmConfig.ts
+++ src/pages/getCmConfig.ts
@@ -1,12 +1,7 @@
 import { CmConfig } from "../services/CmConfig";
 // @ts-expect-error - no types for cmConfig
-import cmConfig from "../../../demo-next/cm.config.ts";
+import cmConfig from "/cm.config.ts";
 
 export const getCmConfig = (): CmConfig => {
   return cmConfig;
 };
-
-// export const getCmConfig = (): CmConfig => {
-//   // console.log(require("../../../demo-next/cm.config.ts"));
-//   return require("../../../demo-next/cm.config.ts").default;
-// };

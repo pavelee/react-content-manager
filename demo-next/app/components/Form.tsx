200539fb7bd3c7370a4140517625a0214447de02
Paweł Ciosek <ciosek.pawel@gmail.com>
2024-10-28 14:43:13 +0100

    feat: enhance Form component with a controlled input for limit and submit functionality, improving user interaction and type safety

--- demo-next/app/components/Form.tsx
+++ demo-next/app/components/Form.tsx
@@ -1,17 +1,42 @@
 "use client";
 
 import { useState } from "react";
+import { TextBlockProps } from "./TextBlock";
 
 interface ComponentForm {
   setProps: (props: any) => void;
 }
 
-const Form = (props: any) => {
+const Form = (props: TextBlockProps & ComponentForm) => {
+  const [limit, setLimit] = useState(props.limit);
+
   return (
     <>
-      <div className="bg-black p-5">
-        <div>Hello!</div>
-      </div>
+      <form className="p-5 flex flex-col gap-4"
+        onSubmit={(e) => {
+          e.preventDefault();
+          const formData = new FormData(e.target as HTMLFormElement);
+          const limit = formData.get("limit") as string;
+          props.setProps({ limit: limit });
+        }}
+      >
+        <div>
+          <input
+            type="number"
+            value={limit}
+            onChange={(e) => setLimit(parseInt(e.target.value))}
+            className="bg-white p-2 rounded-lg border border-gray-300"
+          />
+        </div>
+        <div>
+          <button
+            type="submit"
+            className="bg-blue-500 text-white p-2 rounded-lg"
+          >
+            Save
+          </button>
+        </div>
+      </form>
     </>
   );
 };

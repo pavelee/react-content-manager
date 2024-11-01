3196b13a18c39c81a5341022079fbdfb0d41bf28
Paweł Ciosek <ciosek.pawel@gmail.com>
2024-11-01 21:59:00 +0100

    feat: add AsyncCMComponent for async loading in client-side rendering contexts, improving compatibility with Vite and Next.js

---
+++ src/components/component/AsyncCMComponent.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import React from "react";
+import { useEffect, useState } from "react";
+import { CMComponent, CMComponentProps } from "./CMComponent";
+
+/**
+ * Component that asynchronously loads a CMComponent when using client side rendering.
+ * Use this instead of CMComponent when you develop with Vite or next.js page that is set as client side rendering.
+ * @param props 
+ * @returns 
+ */
+export const AsyncCMComponent = (props: CMComponentProps) => {
+  const { configId, componentId, mode } = props;
+
+  const [component, setComponent] = useState<React.ReactNode>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    let isMounted = true;
+
+    CMComponent({ configId, componentId, mode })
+      .then((element) => {
+        if (isMounted) {
+          setComponent(element);
+        }
+      })
+      .catch((err) => {
+        if (isMounted) {
+          setError(err.message);
+        }
+      });
+
+    return () => {
+      isMounted = false;
+    };
+  }, [configId, componentId, mode]);
+
+  if (error) {
+    throw new Error(error);
+  }
+
+  return <>{component}</>;
+}

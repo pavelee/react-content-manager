4383c644d48267c8bf5f09e68542bb4771567c5f
Paweł Ciosek <pciosek@wip.pl>
2024-12-31 13:05:59 +0100

    feat: update to react-content-manager v3.0.0 and refactor component imports

--- demo/demo-next/next.config.js
+++ demo/demo-next/next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    const path = require("path");
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "cm.config.ts": path.resolve(__dirname, "cm.config.ts"),
+      "cm.fetcher.ts": path.resolve(__dirname, "cm.fetcher.ts"),
+      "cm.persister.ts": path.resolve(__dirname, "cm.persister.ts"),
+    };
+    return config;
+  },
+};
 
 module.exports = nextConfig;
--- demo/demo-vite/src/App.tsx
+++ demo/demo-vite/src/App.tsx
@@ -1,17 +1,17 @@
 import "./App.css";
 import { CMProvider } from "react-content-manager";
-import { AsyncCMComponent } from "react-content-manager/dist/esm/client/AsyncCMComponent";
+import { ClientCMComponent } from "react-content-manager/dist/esm/client";
 
 function App() {
   return (
     <CMProvider mode={"edit"}>
-      <AsyncCMComponent configId="123" componentId={"vite"} mode={"edit"} />
-      <AsyncCMComponent
+      <ClientCMComponent configId="123" componentId={"vite"} mode={"edit"} />
+      <ClientCMComponent
         configId="1234"
         componentId={"cat-facts"}
         mode={"edit"}
       />
-      <AsyncCMComponent
+      <ClientCMComponent
         configId="12312321"
         componentId={"container"}
         mode={"edit"}
